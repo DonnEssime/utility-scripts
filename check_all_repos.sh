005374d3b59619915932c64f0a2438931276a162
Simon Donne <donnesimon@gmail.com>
2016-01-07 10:40:55 +0100

    update on format and included summary at the end for large sets of repos

--- check_all_repos.sh
+++ check_all_repos.sh
@@ -6,27 +6,53 @@ normal=$(tput sgr0)
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 cwd=$(pwd)
-#simply change Desktop/Research/ to the folder that should be crawled
-for d in $(find Desktop/Research/ -name .git -type d -exec dirname {} \;) ; do
+anyunmerged=false
+anyuncommitted=false
+anyunpushed=false
+
+#simply change code/ to the folder that should be crawled
+for d in $(find code/ -name .git -type d -exec dirname {} \;) ; do
 	echo "${bold}$d${normal}"
 	cd "$d"
 	git remote update &> /dev/null
 	if [ -n "$(git log HEAD..origin/master --oneline)" ];
 	then
 		echo "${red}Unmerged upstream changes!${normal}"
+		anyunmerged=true
 	else
-		echo "${green}Merged all upstream changes.${normal}"
+		echo "Merged all upstream changes."
 	fi
 	if [ -n "$(git status -s)" ]; then
 		echo "${red}Uncommitted changes!${normal}"
+		anyuncommitted=true
 	else
-		echo "${green}No uncommitted changes.${normal}"
+		echo "No uncommitted changes."
 	fi
 	if [ -n "$(git status | grep up-to-date)" ]; then
-		echo "${green}Pushed all downstream changes.${normal}"
+		echo "Pushed all downstream changes."
 	else
+		anyunpushed=true
 		echo "${red}Unpushed downstream changes!${normal}"
 	fi
 	cd "$cwd"
 done
+echo ""
+echo ""
+if [ "$anyunmerged" == true ] ; then
+	echo "${red}There are repositories with upstream changes!${normal}"
+fi
+if [ "$anyuncommitted" == true ] ; then
+	echo "${red}There are repositories with uncommitted changes!${normal}"
+fi
+if [ "$anyunpushed" == true ] ; then
+	echo "${red}There are repositories with unpushed changes!${normal}"
+fi
+if [ "$anyunpushed" == false ] ; then
+if [ "$anyuncommitted" == false ] ; then
+if [ "$anyunmerged" == false ] ; then
+	echo "${bold}${green}All repositories are perfectly synchronized!${normal}"
+fi
+fi
+fi
+
 IFS=$SAVEIFS
